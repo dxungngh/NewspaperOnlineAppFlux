6774d5e38e4ab086d7160913fef1d927a5bc568d
Daniel Nguyen <dxungngh@gmail.com>
2016-02-02 16:15:47 +0700

    refactor loading dialog

--- app/src/main/java/com/daniel/flux/newspaper/view/widget/LoadingDialog.java
+++ app/src/main/java/com/daniel/flux/newspaper/view/widget/LoadingDialog.java
@@ -9,9 +9,11 @@ import android.app.Dialog;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.daniel.flux.newspaper.R;
 
@@ -19,12 +21,14 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class LoadingDialog extends Dialog {
+    private static final String TAG = LoadingDialog.class.getSimpleName();
+
     private static LoadingDialog loadingDialog;
 
-    @Bind(R.id.loading)
+    @Bind(R.id.dialog_loading_layout)
+    protected RelativeLayout dialogLayout;
+    @Bind(R.id.dialog_loading_image)
     protected ImageView loadingImageView;
-
-    private static final String TAG = LoadingDialog.class.getSimpleName();
 
     public static LoadingDialog getInstance(Activity activity) {
         if (loadingDialog == null) {
@@ -60,10 +64,22 @@ public class LoadingDialog extends Dialog {
     }
 
     private void showLoading() {
+        this.dialogLayout.setVisibility(View.VISIBLE);
         this.loadingImageView.setVisibility(View.VISIBLE);
         this.loadingImageView.setImageResource(R.drawable.loading);
         AnimationDrawable loadAnimation = (AnimationDrawable) this.loadingImageView.getDrawable();
-        this.loadingImageView.setVisibility(View.VISIBLE);
         loadAnimation.start();
+    }
+
+    @Override
+    public void dismiss() {
+        Log.i(TAG, "dismiss");
+        AnimationDrawable loadAnimation = (AnimationDrawable) this.loadingImageView.getDrawable();
+        loadAnimation.stop();
+
+        this.loadingImageView.setVisibility(View.GONE);
+        this.dialogLayout.setVisibility(View.GONE);
+
+        super.dismiss();
     }
 }
